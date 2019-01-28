6dce769d291924a214c0555336a97374d15c891d
Tyrel Hiebert <tyrelh@uvic.ca>
2019-01-27 20:09:55 -0800

    adjust WARNING bias

--- app/params.js
+++ app/params.js
@@ -8,15 +8,15 @@ module.exports = {
   INITIAL_FEEDING: 3,
   SURVIVAL_MIN: 50,
   // scores for moves
-  ASTAR_SUCCESS: 8.0, // DOWN from 10
+  ASTAR_SUCCESS: 8.0, // DOWN from 10 -
   ASTAR_ALT: 2.0,
   BASE_KILL_ZONE: 2.1,
   BASE_FOOD: 0.5,
-  BASE_TAIL: 7.9, // UP from 0.35
+  BASE_TAIL: 7.9, // UP from 0.35 +
   BASE_SPACE: 0.3,
-  BASE_WARNING: -3, // DOWN from 0.2 *
-  BASE_DANGER: -7.9, // DOWN from 0.1
-  BASE_ENEMY_HEAD: -11, // DOWN from -2
+  BASE_WARNING: -2.5, // DOWN from 0.2 -+
+  BASE_DANGER: -7.9, // DOWN from 0.1 -
+  BASE_ENEMY_HEAD: -11, // DOWN from -2 -
   BASE_BAD: -10.0,
-  BASE_PREVIOUS: 0.09 // DOWN from 2
+  BASE_PREVIOUS: 0.09 // DOWN from 2 -
 }
