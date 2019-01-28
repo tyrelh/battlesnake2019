6f03d15435745df661769831b240eb898c7662e1
Tyrel Hiebert <tyrelh@uvic.ca>
2019-01-27 22:18:26 -0800

    add enemy_distance score, decrease survival_min

--- app/params.js
+++ app/params.js
@@ -6,17 +6,17 @@ module.exports = {
   CONSOLE_LOG: true,
   // basic game params
   INITIAL_FEEDING: 3,
-  SURVIVAL_MIN: 50,
+  SURVIVAL_MIN: 40,     // DOWN from 50 -
   // scores for moves
-  ASTAR_SUCCESS: 8.0, // DOWN from 10 -
-  ASTAR_ALT: 2.0,
+  ASTAR_SUCCESS: 8.0,   // DOWN from 10 -
+  ENEMY_DISTANCE: 1.5,
   BASE_KILL_ZONE: 2.1,
   BASE_FOOD: 0.5,
-  BASE_TAIL: 7.9, // UP from 0.35 +
+  BASE_TAIL: 7.9,       // UP from 0.35 +
   BASE_SPACE: 0.3,
-  BASE_WARNING: -2.5, // DOWN from 0.2 -+
-  BASE_DANGER: -8.9, // DOWN from 0.1 --
+  BASE_WARNING: -2.5,   // DOWN from 0.2 -+
+  BASE_DANGER: -8.9,    // DOWN from 0.1 --
   BASE_ENEMY_HEAD: -11, // DOWN from -2 -
   BASE_BAD: -10.0,
-  BASE_PREVIOUS: 0.09 // DOWN from 2 -
+  BASE_PREVIOUS: 0.09   // DOWN from 2 -
 }
