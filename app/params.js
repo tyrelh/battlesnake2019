3e4536a8d5736960621a02b6b8d7c378b1d19e4d
Tyrel Hiebert <tyrelh@uvic.ca>
2019-01-27 21:40:55 -0800

    decrease danger bias

--- app/params.js
+++ app/params.js
@@ -15,7 +15,7 @@ module.exports = {
   BASE_TAIL: 7.9, // UP from 0.35 +
   BASE_SPACE: 0.3,
   BASE_WARNING: -2.5, // DOWN from 0.2 -+
-  BASE_DANGER: -7.9, // DOWN from 0.1 -
+  BASE_DANGER: -8.9, // DOWN from 0.1 --
   BASE_ENEMY_HEAD: -11, // DOWN from -2 -
   BASE_BAD: -10.0,
   BASE_PREVIOUS: 0.09 // DOWN from 2 -
