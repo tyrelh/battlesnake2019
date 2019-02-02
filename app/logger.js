8710666a97105a45b5b7acb525b9dd0e65fb57ef
Tyrel Hiebert <tyrelh@uvic.ca>
2019-02-02 12:23:03 -0800

    revert ts project back to js project

--- app/logger.js
+++ app/logger.js
@@ -1,50 +1,52 @@
-var fs = require("fs");
-var p = require("./params");
-var log = "";
-var initGameLogs = function () {
-    log = "";
-};
+const fs = require("fs");
+const p = require("./params");
+
+let log = "";
+
+const initGameLogs = () => {
+  log = "";
+}
+
 // write logs for game to file and update the index of logs
-var writeLogs = function (data) {
-    var gameId = data.game.id;
-    var path = __dirname + "/../logs/" + gameId + ".txt";
-    // write log
-    fs.writeFile(path, log, function (err) {
-        if (err)
-            return console.log("There was an error saving the logs: " + err);
-        console.log("The log for game " + gameId + " was saved.");
-        // update index of logs
-        fs.readFile(__dirname + "/../logs/index.html", "utf8", function (err, contents) {
-            // console.log(contents)
-            var newEntry = "<a href=\"/logs/" + gameId + ".txt\">GAME: " + gameId + "</a><br />";
-            var newIndex = contents + "\n" + newEntry;
-            fs.writeFile(__dirname + "/../logs/index.html", newIndex, function (err) {
-                if (err)
-                    return console.log("There was an error saving the new index.html: " + err);
-                console.log("The logs index.html was updated");
-            });
-        });
+const writeLogs = (data) => {
+  const gameId = data.game.id;
+  const path = `${__dirname}/../logs/${gameId}.txt`;
+  // write log
+  fs.writeFile(path, log, err => {
+    if (err) return console.log(`There was an error saving the logs: ${err}`);
+    console.log(`The log for game ${gameId} was saved.`);
+    // update index of logs
+    fs.readFile(`${__dirname}/../logs/index.html`, "utf8", (err, contents) => {
+      // console.log(contents)
+      const newEntry = `<a href="/logs/${gameId}.txt">GAME: ${gameId}</a><br />`;
+      const newIndex = contents + "\n" + newEntry;
+      fs.writeFile(`${__dirname}/../logs/index.html`, newIndex, err => {
+        if (err) return console.log(`There was an error saving the new index.html: ${err}`);
+        console.log("The logs index.html was updated");
+      })
     });
-};
-var error = function (message) {
-    log += "ERROR: " + message + "\n";
-    if (p.CONSOLE_LOG)
-        console.log("ERROR: " + message);
-};
-var status = function (message) {
-    log += message + "\n";
-    if (p.CONSOLE_LOG)
-        console.log("" + message);
-};
-var debug = function (message) {
-    log += "DEBUG: " + message + "\n";
-    if (p.CONSOLE_LOG)
-        console.log("DEBUG: " + message);
-};
+  });
+}
+
+const error = message => {
+  log += `ERROR: ${message}\n`
+  if (p.CONSOLE_LOG) console.log(`ERROR: ${message}`);
+}
+
+const status = message => {
+  log += `${message}\n`
+  if (p.CONSOLE_LOG) console.log(`${message}`);
+}
+
+const debug = message => {
+  log += `DEBUG: ${message}\n`
+  if (p.CONSOLE_LOG) console.log(`DEBUG: ${message}`);
+}
+
 module.exports = {
-    initGameLogs: initGameLogs,
-    writeLogs: writeLogs,
-    error: error,
-    status: status,
-    debug: debug
-};
+  initGameLogs: initGameLogs,
+  writeLogs: writeLogs,
+  error: error,
+  status: status,
+  debug: debug
+}
--- app/target.js
+++ app/target.js
@@ -1,41 +1,44 @@
-var k = require("./keys");
-var g = require("./grid");
-var log = require("./logger");
-var closestFood = function (grid, self) {
-    return closestTarget(grid, self, k.FOOD);
+const k = require("./keys");
+const g = require("./grid");
+const log = require("./logger")
+
+const closestFood = (grid, self) => {
+  return closestTarget(grid, self, k.FOOD);
 };
-var closestKillableEnemy = function (grid, self) {
-    return closestTarget(grid, self, k.KILL_ZONE);
-};
-var closestEnemyHead = function (grid, self) {
-    return closestTarget(grid, self, k.ENEMY_HEAD);
-};
+
+const closestKillableEnemy = (grid, self) => {
+  return closestTarget(grid, self, k.KILL_ZONE);
+}
+
+const closestEnemyHead = (grid, self) => {
+  return closestTarget(grid, self, k.ENEMY_HEAD);
+}
+
 // simple search for closest target of a specified grid type
-var closestTarget = function (grid, self, targetType) {
-    try {
-        var closestTarget_1 = null;
-        var closestDistance = 9999;
-        for (var i = 0; i < grid.length; i++) {
-            for (var j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] === targetType) {
-                    var target = { x: j, y: i };
-                    var distance = g.getDistance(self, target);
-                    if (distance < closestDistance) {
-                        closestTarget_1 = target;
-                        closestDistance = distance;
-                    }
-                }
-            }
+const closestTarget = (grid, self, targetType) => {
+  try {
+    let closestTarget = null;
+    let closestDistance = 9999;
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] === targetType) {
+          const target = { x: j, y: i };
+          const distance = g.getDistance(self, target);
+          if (distance < closestDistance) {
+            closestTarget = target;
+            closestDistance = distance;
+          }
         }
-        return closestTarget_1;
+      }
     }
-    catch (e) {
-        log.error("ex in target.closestTarget \" + " + e);
-    }
-    return null;
-};
+    return closestTarget;
+  }
+  catch (e) { log.error(`ex in target.closestTarget " + ${e}`); }
+  return null
+}
+
 module.exports = {
-    closestFood: closestFood,
-    closestKillableEnemy: closestKillableEnemy,
-    closestEnemyHead: closestEnemyHead
+  closestFood: closestFood,
+  closestKillableEnemy: closestKillableEnemy,
+  closestEnemyHead: closestEnemyHead
 };
