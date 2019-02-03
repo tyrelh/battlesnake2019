d944d981ea9d4fc2c18355f310c156cdf226f92d
Tyrel Hiebert <tyrelh@uvic.ca>
2019-02-03 08:49:41 -0800

    exception accumulation for easier reading

--- app/logger.js
+++ app/logger.js
@@ -2,42 +2,60 @@ const fs = require("fs");
 const p = require("./params");
 
 let log = "";
+let exLog = "############################# EXCEPTIONS\n";
 
 const initGameLogs = () => {
   log = "";
+  exLog = "############################# EXCEPTIONS\n";
 }
 
 // write logs for game to file and update the index of logs
 const writeLogs = (data) => {
   const gameId = data.game.id;
   const path = `${__dirname}/../logs/${gameId}.txt`;
+  // append game exeptions to end of log for easy viewing
+  log += "\n" + exLog;
   // write log
-  fs.writeFile(path, log, err => {
-    if (err) return console.log(`There was an error saving the logs: ${err}`);
-    console.log(`The log for game ${gameId} was saved.`);
-    // update index of logs
-    fs.readFile(`${__dirname}/../logs/index.html`, "utf8", (err, contents) => {
-      // console.log(contents)
-      const newEntry = `<a href="/logs/${gameId}.txt">GAME: ${gameId}</a><br />`;
-      const newIndex = contents + "\n" + newEntry;
-      fs.writeFile(`${__dirname}/../logs/index.html`, newIndex, err => {
-        if (err) return console.log(`There was an error saving the new index.html: ${err}`);
-        console.log("The logs index.html was updated");
-      })
-    });
-  });
+  fs.writeFile(
+    path,
+    log,
+    err => {
+      if (err) return console.log(`There was an error saving the logs: ${err}`);
+      console.log(`The log for game ${gameId} was saved.`);
+      // update index of logs
+      // read current index
+      fs.readFile(
+        `${__dirname}/../logs/index.html`,
+        "utf8",
+        (err, contents) => {
+          // append new entry
+          const newEntry = `<a href="/logs/${gameId}.txt">GAME: ${gameId}</a><br />`;
+          const newIndex = contents + "\n" + newEntry;
+          // write updated index
+          fs.writeFile(
+            `${__dirname}/../logs/index.html`,
+            newIndex,
+            err => {
+              if (err) return console.log(`There was an error saving the new index.html: ${err}`);
+              console.log("The logs index.html was updated");
+            }
+          )
+        }
+      );
+    }
+  );
 }
 
-const error = message => {
+// debug levels
+const error = (message, turn = null) => {
   log += `ERROR: ${message}\n`
   if (p.CONSOLE_LOG) console.log(`ERROR: ${message}`);
+  if (turn != null) exLog += `EX ON TURN ${turn}: ${message}\n`
 }
-
 const status = message => {
   log += `${message}\n`
   if (p.CONSOLE_LOG) console.log(`${message}`);
 }
-
 const debug = message => {
   log += `DEBUG: ${message}\n`
   if (p.CONSOLE_LOG) console.log(`DEBUG: ${message}`);
