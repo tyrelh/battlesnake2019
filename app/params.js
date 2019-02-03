78e927c12c1b1b7499e5563ef799a914012fac72
Tyrel Hiebert <tyrelh@uvic.ca>
2019-02-02 17:13:52 -0800

    add initial time kill and forget about it score bias

--- app/params.js
+++ app/params.js
@@ -1,3 +1,4 @@
+const SURVIVAL_MIN = 30 // started @ 50   --
 module.exports = {
   // logging
   DEBUG: true,
@@ -6,18 +7,20 @@ module.exports = {
   CONSOLE_LOG: true,
   // basic game params
   INITIAL_FEEDING: 3,
-  SURVIVAL_MIN: 30,     // down from 50   --
+  SURVIVAL_MIN: SURVIVAL_MIN,
+  INITIAL_TIME_KILL: 100 - SURVIVAL_MIN,
   // scores for moves
-  ASTAR_SUCCESS: 8.0,   // down from 10   -
+  ASTAR_SUCCESS: 9.0,   // started @ 10   -+
   ENEMY_DISTANCE: 1.5,
   BASE_KILL_ZONE: 2.1,
   BASE_FOOD: 0.9,
-  BASE_TAIL: 7.9,       // up from    0.4 +
-  BASE_SPACE: 0.2,      // down from  0.1 -
-  BASE_WALL_NEAR: -0.7, // down from -0.1 ---
-  BASE_WARNING: -2.5,   // down from  0.2 -+
-  BASE_DANGER: -8.9,    // down from  0.1 --
-  BASE_ENEMY_HEAD: -11, // down from -2   -
+  BASE_TAIL: 7.9,       // started @  0.4 +
+  BASE_SPACE: 0.2,      // started @  0.1 -
+  BASE_WALL_NEAR: -0.9, // started @ -0.1 ----
+  BASE_WARNING: -2.5,   // started @  0.2 -+
+  BASE_DANGER: -8.9,    // started @  0.1 --
+  BASE_ENEMY_HEAD: -11, // started @ -2   -
   BASE_BAD: -10.0,
-  BASE_PREVIOUS: 0.09   // down from  2   -
+  BASE_PREVIOUS: 0.09,   // started @  2   -
+  FORGET_ABOUT_IT: -100
 }
