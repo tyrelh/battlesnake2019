ecb0e2302c1bfb63dba9fa41673c40ddcba9200d
Tyrel Hiebert <tyrelh@uvic.ca>
2019-02-03 20:41:27 -0800

    tweak base_wall_near & enemy_distance score biases

--- app/params.js
+++ app/params.js
@@ -7,19 +7,19 @@ module.exports = {
   CONSOLE_LOG: true,
   // basic game params
   INITIAL_FEEDING: 5,
-  SURVIVAL_MIN: SURVIVAL_MIN,
-  INITIAL_TIME_KILL: 100 - SURVIVAL_MIN,
+  SURVIVAL_MIN: 30,      // started @ 50   --
+  INITIAL_TIME_KILL: 65, // started @ 70   -
   // scores for moves
-  ASTAR_SUCCESS: 9.1,   // started @ 10   -++
-  ENEMY_DISTANCE: 1.5,
+  ASTAR_SUCCESS: 9.1,    // started @ 10   -++
+  ENEMY_DISTANCE: 0.99,
   BASE_KILL_ZONE: 2.1,
   BASE_FOOD: 0.9,
-  BASE_TAIL: 7.9,       // started @  0.4 +
-  BASE_SPACE: 0.2,      // started @  0.1 -
-  BASE_WALL_NEAR: -0.9, // started @ -0.1 ----
-  BASE_WARNING: -2.6,   // started @  0.2 -+-
-  BASE_DANGER: -8.9,    // started @  0.1 --
-  BASE_ENEMY_HEAD: -11, // started @ -2   -
+  BASE_TAIL: 7.9,        // started @  0.4 +
+  BASE_SPACE: 0.2,       // started @  0.1 -
+  BASE_WALL_NEAR: -1,    // started @ -0.1 -----
+  BASE_WARNING: -2.6,    // started @  0.2 -+-
+  BASE_DANGER: -8.9,     // started @  0.1 --
+  BASE_ENEMY_HEAD: -11,  // started @ -2   -
   BASE_BAD: -10.0,
   BASE_PREVIOUS: 0.09,   // started @  2   -
   FORGET_ABOUT_IT: -100
