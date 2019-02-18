130b171343dd879b8414533c497e5b1f6c1af521
Tyrel Hiebert <tyrelh@uvic.ca>
2019-02-18 14:39:59 -0800

    adjust several params

--- app/params.js
+++ app/params.js
@@ -6,21 +6,21 @@ module.exports = {
   CONSOLE_LOG: true,
 
   // basic game params
-  INITIAL_FEEDING: 12,    // started @  5   +++++
-  SURVIVAL_MIN: 45,      // started @ 50   --+++
-  INITIAL_TIME_KILL: 42, // started @ 70   --------
-  WALL_NEAR_BASE_MOVE_MULTIPLIER: 11, // started @ 1.6 ++++++++++++
+  INITIAL_FEEDING: 15,    // started @  5   ++++++
+  SURVIVAL_MIN: 47,      // started @ 50   --+++++
+  INITIAL_TIME_KILL: 24, // started @ 70   --------------
+  WALL_NEAR_BASE_MOVE_MULTIPLIER: 12, // started @ 1.6 +++++++++++++
   WALL_NEAR_FILL_MULTIPLIER: 0.2, // started @ 0.4 --
 
   // scores for moves
   ASTAR_SUCCESS: 9.2,    // started @ 10   -++
   SUGGEST_MOVE: 0.2,
-  ENEMY_DISTANCE: 2,   // started @ 0.99 +-+
-  WALL_DISTANCE: 2,
+  ENEMY_DISTANCE: 1.9,   // started @ 0.99 +-+++-
+  WALL_DISTANCE: 2.1,    // started @ 2    +
   BASE_KILL_ZONE: 2,     // started @ 2.1  -
   BASE_FOOD: 0.8,        // started @ 0.8  -
   BASE_TAIL: 7.9,        // started @  0.4 +
-  BASE_SPACE: 0.41,       // started @  0.2 -+++
+  BASE_SPACE: 0.40,       // started @  0.2 -+++-
   BASE_WALL_NEAR: -1.1,  // started @ -0.1 ------
   BASE_WARNING: -2.6,    // started @  0.2 -+-
   BASE_DANGER: -10.0,     // started @  0.1 ----
